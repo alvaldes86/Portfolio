27e2889e42add132a960a1559694a6c556f0fa52
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2022-03-04 22:34:18 -0500

    updated index

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const $ = (id) => {return document.getElementById(id)}
 
 const changeName = () => {
     $("devName").onmouseover = () => {
-        $("devName").innerHTML = "Software Developer";  
+        $("devName").innerHTML = "Software Developer";
+
+        if(!$("devName").classList.contains("title-animation")) {
+            $("devName").classList.add("title-animation");
+        } 
+
     } 
     $("devName").onmouseleave = () => {
         $("devName").innerHTML = "Yohan Valdes";
