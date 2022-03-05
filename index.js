1c57cb0428ddd9dd480c48f4c7765de24b2b327f
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2022-03-05 12:42:55 -0500

    added function addName and addOccupation

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 "use strict"
 
 
-const $ = (id) => {return document.getElementById(id)}
+const $ = (id) => {return document.getElementById(id)};           
 
+const addOccupation = (str) => {
+    let arrName = [];
+    let occupation = ["S", "o", "f", "t", "w", "a", "r", "e", " ", "D", "e", "v", "e", "l", "o", "p", "e", "r"];
 
+    //gets the devName
+    for(let i = 0; i < str.length; i++) {
+        arrName.push(str[i]);
+    }
 
-window.onload = () => {
+    let myInterval = setInterval(()=>{
+        //if statement to delete the name
+        if(arrName.length >= 1) {
+            arrName.pop();
+            let newName = ""
+            for(let i = 0 ;i < arrName.length; i++) {
+                newName += arrName[i];
+            }
+            $("devName").innerHTML = newName;
+        } else {
+            clearInterval(myInterval);
+            //once empty add the occupation
+            if($("devName").innerHTML.length == 0) {
+                console.log("add occupation");
+                let mySecInterval = setInterval(()=>{
+                    if(occupation.length >= 1) {
+                        $("devName").innerHTML += occupation[0];
+                        occupation.shift();
+                    }
+                    if(occupation.length == 0) {
+                        clearInterval(mySecInterval);
+                    }
+                }, 125);
+            }
+        }
+    }, 125)
+}
 
+const addName = () => {
     let name = ["Y", "o", "h", "a", "n", " ", "V", "a", "l", "d", "e", "s"];
     
     let myInterval = setInterval(()=>{
@@ -16,6 +50,13 @@ window.onload = () => {
         }
         if(name.length == 0) {
             clearInterval(myInterval);
+            addOccupation($("devName").innerHTML);
         }
     }, 125);
+}
+
+
+window.onload = () => {
+    addName();
+    addOccupation($("devName").innerHTML);
 }
